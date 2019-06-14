44368fa6ebaef481345c5cbe180e26228425d3df
miao <1121333722@qq.com>
2019-06-14 11:15:14 +0800

    Default Changelist

--- src/main/java/com/nf/entity/test.java
+++ src/main/java/com/nf/entity/test.java
@@ -3,14 +3,9 @@ package com.nf.entity;
 import java.io.Console;
 
 public class test {
-
-
     public static void main(String[] args) {
-            System.out.println();
+        System.out.println();
+        System.out.println("!");
+        System.err.println("!");
     }
-
-
-
-
-
 }
